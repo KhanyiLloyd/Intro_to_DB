e117c731348bac00fa882abe03a9077cd7f8a854
Nokukhanya Lloyd <lloydkhanyi@gmail.com>
2025-08-17 16:50:22 +0200

    Add files via upload

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,5 @@
+INSERT INTO customer
+
 USE alx_book_store;
 
-INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
